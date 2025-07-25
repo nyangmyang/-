1220ac539dbc9477b54f79f68d4b0eeb342172e7
nyangmyang <koyehyeon2001@gmail.com>
2025-07-25 11:49:58 +0900

    Revert "경로 수정"
    This reverts commit 25b77fdd6125024a8bb8d54a96c32de7568bc4e0.

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 import sys, os
 import uvicorn
+import os, sys
 
-# sys.path 설정을 맨 위로 이동
-base_dir = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(base_dir + "/back")  # back 폴더를 추가
-
-from app.main import app  # 이제 app.main을 찾을 수 있음
+file_path = os.path.abspath("app/main.py")
+uvicorn.run(file_path + ":app", ...)
 
 if __name__ == "__main__":
     uvicorn.run(
@@ -14,4 +12,4 @@ if __name__ == "__main__":
         port=8000,
         reload=True,
         log_level="info"
-    )
+    ) 
